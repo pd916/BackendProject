c226804d2b1503264a6172088fd02779eeffaa27
pd <pdbaginz@gmail.com>
2024-01-08 09:50:46 +0500

    util code discustion

--- src/index.js
+++ src/index.js
@@ -12,7 +12,15 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port : ${process.env.PORT}`)
+    })
 
+})
+.catch((err)=> {
+    console.log("MONGODB connection fail !!!", err)
+})
 
 
 
