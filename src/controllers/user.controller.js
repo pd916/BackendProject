4337e962a8f09dd9361d97b8790edaf0f1f5202f
pd <pdbaginz@gmail.com>
2024-01-14 09:18:16 +0500

    add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -164,7 +164,7 @@ const logoutUser = asyncHandler(async (req, res)=> {
 const refreshAccessToken = asyncHandler(async (req, res)=> {
    const incomingRefreshToken = req.cookies.refereshToken || req.body.refereshToken
 
-   if(incomingRefreshToken) {
+   if(!incomingRefreshToken) {
       throw new ApiError(401, "unauthorized request")
    }
 
